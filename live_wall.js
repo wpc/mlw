fd054b65a0b4eba50b388c7e85e154abfcde2656
wpc <alex.hal9000@gmail.com>
2013-03-13 00:13:30 -0700

    feeds url extraction

--- live_wall.js
+++ live_wall.js
@@ -1,4 +1,24 @@
-var mlv = {};
+var mlw = ("object" == typeof exports) ? exports : {};
 
-mlv.extract_feeds_url = function(url) {
+mlw.feeds = (function() {
+    var extractURL = function(gridViewUrl) {
+        var matches = gridViewUrl.match(/\/projects\/([^\/]+)/i);
+        if(!matches) {
+            return null;
+        }
+        var projectId = matches[1];
+        return gridViewUrl.replace(/\/projects\/.*$/i, "/api/v2/projects/" + projectId + "/feeds/events.xml");
+    };
+
+    return {
+        extractURL: extractURL
+    }
+})();
+
+
+if( "object" == typeof document ) {
+    jQuery(document).ready(function($) {
+        var feedUrl = mlw.feeds.extractURL(window.location.href);
+        console.log("feedsUrl = " + feedsUrl);
+    });
 }
---
+++ test.js
@@ -0,0 +1,22 @@
+var mlw = require('./live_wall.js');
+
+exports.feeds = {
+    extract_url_from_grid_view_url: function(test) {
+        var url = mlw.feeds.extractURL("https://mingle.com/projects/foo/cards/grid?color_by=type");
+        test.equal(url,  "https://mingle.com/api/v2/projects/foo/feeds/events.xml")
+        test.done();
+    },
+
+    extract_url_from_url_with_context_path: function(test) {
+        var url = mlw.feeds.extractURL("https://mingle.com/sbb/projects/foo/cards/grid?color_by=type");
+        test.equal(url,  "https://mingle.com/sbb/api/v2/projects/foo/feeds/events.xml")
+        test.done();
+    },
+
+    extract_url_should_return_null_if_url_is_not_a_grid_view_url: function(test) {
+        var url = mlw.feeds.extractURL("https://go01.com/go/home")
+        test.equal(url, null);
+        test.done();
+    }
+
+}
