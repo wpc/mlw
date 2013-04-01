a3a55005d5ee58b6eac92522b62ea0b69b544172
wpc <alex.hal9000@gmail.com>
2013-04-01 07:50:17 -0700

    feeds polling and basic parsing

--- live_wall.js
+++ live_wall.js
@@ -4,25 +4,117 @@ if ("object" == typeof exports) {
     jQuery = require('jquery');
 }
 
-mlw.feeds = (function($) {
-    var extractURL = function(gridViewUrl) {
-        var matches = gridViewUrl.match(/\/projects\/([^\/]+)/i);
+mlw.mingle = (function() {
+    function extractFeedUrl(projecturl) {
+        var matches = projecturl.match(/\/projects\/([^\/]+)/i);
         if(!matches) {
             return null;
         }
         var projectId = matches[1];
-        return gridViewUrl.replace(/\/projects\/.*$/i, "/api/v2/projects/" + projectId + "/feeds/events.xml");
-    };
+        return projecturl.replace(/\/projects\/.*$/i, "/api/v2/projects/" + projectId + "/feeds/events.xml");
+    }
 
     return {
-        extractURL: extractURL
+        extractFeedUrl:extractFeedUrl
     }
-})(jQuery);
+})();
+
+
+
+mlw.feeds = (function(jQuery, setTimeout) {
+    var _latestPage = null;
+    var _reqSettings = null;
+    var _latestEntryId = null;
+
+    var Entry = function(element) {
+        return {
+            id: function() { return element.find('id').text(); }
+        };
+    }
+
+
+    var EntryCollection = function(elementCollection) {
+        return {
+            first: function() { return Entry(elementCollection.first()); },
+            each: function(iterator) {
+                elementCollection.each(function() {
+                    return iterator(Entry(jQuery(this)));
+                });
+            }
+        };
+    };
+
+    function request(url, delegate, options) {
+        var s = {url: url,
+		type: "GET",
+		dataType: "text",
+		async: true,
+                error: function(jqXHR, textStatus, errorThrown) {
+                    console.log("!!!!!!!!!!!!!!")
+                    console.log(errorThrown)
+                    delegate(null)
+                },
+                success: function(xml) {
+                    var document = jQuery(jQuery.parseXML(xml));
+                    var entryElements = document.find('feed entry');
+                    delegate(EntryCollection(entryElements));
+                }};
+
+        jQuery.extend(s, _reqSettings);
+        jQuery.extend(s, options);
+        jQuery.ajax(s);
+    }
+
+    function poll(delegate, interval) {
+        var continuePolling = function() {
+            setTimeout(function() { poll(delegate, interval); }, interval);
+        }
+
+        request(_latestPage, function(entries) {
+            var newEntries = [];
+            if (entries) {
+                var latestEntryId = entries.first().id();
+
+                entries.each(function(entry) {
+                    if( entry.id() === _latestEntryId ) {
+                       _latestEntryId = latestEntryId;
+                        delegate(newEntries);
+                        return false;
+                    } else {
+                        newEntries.push(entry);
+                    }
+
+                })
+            }
+            continuePolling();
+        });
+    }
+
+    function init(url, reqSettings) {
+        _latestPage = url;
+        _reqSettings = reqSettings;
+        request(_latestPage, function(entries) {
+            _latestEntryId = entries.first().id();
+        }, {async: false});
+    }
+
+    return {
+        init: init,
+        poll: poll
+    }
+
+})(jQuery, setTimeout);
+
 
 
 if( "object" == typeof document ) {
     jQuery(document).ready(function($) {
-        var feedUrl = mlw.feeds.extractURL(window.location.href);
-        console.log("feedsUrl = " + feedsUrl);
+        var url  = mlw.mingle.extractFeedUrl(window.location.href)
+        if (url != null) {
+            mlw.feeds.init(url);
+            mlw.feeds.poll(function(newEntries) {
+                $.map(newEntries, function(e) { console.log( "new:" + e.id()); });
+            }, 10000);
+        }
     });
 }
--- test.js
+++ test.js
@@ -1,23 +1,56 @@
-require('jquery')
-var mlw = require('./live_wall.js');
+var mlw = require('./live_wall.js'),
+jQuery = require('jquery'),
+http = require('http'),
+fs = require('fs');
 
-exports.feeds = {
-    extract_url_from_grid_view_url: function(test) {
-        var url = mlw.feeds.extractURL("https://mingle.com/projects/foo/cards/grid?color_by=type");
+exports.mingle = {
+    extract_feed_url_from_grid_view_url: function(test) {
+        var url = mlw.mingle.extractFeedUrl("https://mingle.com/projects/foo/cards/grid?color_by=type");
         test.equal(url,  "https://mingle.com/api/v2/projects/foo/feeds/events.xml")
         test.done();
     },
 
-    extract_url_from_url_with_context_path: function(test) {
-        var url = mlw.feeds.extractURL("https://mingle.com/sbb/projects/foo/cards/grid?color_by=type");
+    extract_feed_url_from_url_with_context_path: function(test) {
+        var url = mlw.mingle.extractFeedUrl("https://mingle.com/sbb/projects/foo/cards/grid?color_by=type");
         test.equal(url,  "https://mingle.com/sbb/api/v2/projects/foo/feeds/events.xml")
         test.done();
     },
 
-    extract_url_should_return_null_if_url_is_not_a_grid_view_url: function(test) {
-        var url = mlw.feeds.extractURL("https://go01.com/go/home")
+    extract_feed_url_should_return_null_if_url_is_not_a_grid_view_url: function(test) {
+        var url = mlw.mingle.extractFeedUrl("https://go01.com/go/home")
         test.equal(url, null);
         test.done();
     }
+}
 
+
+exports.feeds = {
+    setUp: function(callback) {
+      //  var data = this.data = {latest: };
+        // var feedLatestPage = "test/data/latest.atom.xml"
+        // this.server = http.createServer(function (req, res) {
+        //     console.log('*********>> request received');
+        //     fs.readFile(this.feedLatestPage, 'utf8', function(error, data) {
+        //         if(error) {
+        //             res.writeHead(404);
+        //             res.end(JSON.stringify(err));
+        //         }
+        //         res.writeHead(200, {'Content-Type': 'application/atom+xml'});
+        //         res.end(data);
+        //     });
+        // });
+
+        // this.server.listen(8124);
+
+        mlw.feeds.init("http://localhost:3000/api/v2/projects/first/feeds/events.xml", {
+            username: 'admin',
+            password: 'pass'
+        });
+        callback();
+    },
+
+    poll_feeds_get_new_entries: function(test) {
+        mlw.feeds.poll(function(entries) {});
+        test.done();
+    }
 }
